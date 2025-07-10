113a63cb546c3a7f97e42b64b2adcb6efffa6941
Hércules Costa <dev.hrcules@gmail.com>
2025-07-10 14:20:16 -0300

    feat: adicionando o banco de dados e os models

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require("express");
 const app = express();
-
+const db = require("./db/connection");
 const PORT = 3000;
 
 app.listen(PORT, () => {
   console.log("está rodando...");
 });
 
+//db connection
+db.authenticate()
+  .then(() => {
+    console.log("Conectou ao banco com sucesso.");
+  })
+  .catch((err) => {
+    console.log("Ocorreu um erro ao conectar: ", err);
+  });
+
+//routes
 app.get("/", (req, res) => {
   res.send("está funcionando");
 });
